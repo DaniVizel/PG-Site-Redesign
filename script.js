a4c6f6cf04810d492f99d929ea62400315697864
DaniVizel <daniella.vizelter@student.ocadu.ca>
2021-11-04 02:35:57 -0400

    ffff

--- script.js
+++ script.js
@@ -4,20 +4,35 @@ var open = false;
 var sideBarText = "<h4>Sign In</h4> <h4>Your Favourites</h4> <h4>Bookshelves</h4> <h4>About</h4> <h4>FAQ</h4> <h4>Donate</h4>"
 // how would i add classes to the additional elements above?
 
-$("nav").click(expand);
-
-function expand(){
-    // if (open = true){
-    //     $("nav").remove(sideBarText);
-
-    //     open = false;
-    // }
-    // console.log("active");
+$("nav").click(function(){
     $(".sidebar").toggleClass("active");
+    //     $("nav").append(sideBarText);
 
-    $("nav").append(sideBarText);
-    open = true;
+});
+
+
+
+
+
+
+
+
+
+
+
+
+// function expand(){
+//     // if (open = true){
+//     //     $("nav").remove(sideBarText);
+
+//     //     open = false;
+//     // }
+//     // console.log("active");
+//     $(".sidebar").toggleClass("active");
+
+//     $("nav").append(sideBarText);
+//     open = true;
     
 
-}
+// }
 
