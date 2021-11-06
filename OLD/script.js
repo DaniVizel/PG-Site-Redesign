8f4b46344a7645f6a12527b8eb02c9be514af613
DaniVizel <daniella.vizelter@student.ocadu.ca>
2021-11-05 20:46:42 -0400

    starting over

---
+++ OLD/script.js
@@ -0,0 +1,178 @@
+console.log("Project Gutenberg Redesign");
+
+$("nav").click(function(){
+    $(".sidebar").toggleClass("active");
+
+});
+$(".letter").click(function(){
+    $(".dropdown").css({
+        height: '70vh',
+
+});
+});
+// LETTERS
+$(".A").click(function(){
+    $(".letter-highlight").css({
+        'display': 'initial',
+        'margin-left': '240px',
+    });
+});
+$(".B").click(function(){
+    $(".letter-highlight").css({
+        'display': 'initial',
+        'margin-left': '279px',
+    });
+});
+$(".C").click(function(){
+    $(".letter-highlight").css({
+        'display': 'initial',
+        'margin-left': '318px',
+    });
+});
+$(".D").click(function(){
+    $(".letter-highlight").css({
+        'display': 'initial',
+        'margin-left': '355px',
+    });
+});
+$(".E").click(function(){
+    $(".letter-highlight").css({
+        'display': 'initial',
+        'margin-left': '393px',
+    });
+});
+$(".F").click(function(){
+    $(".letter-highlight").css({
+        'display': 'initial',
+        'margin-left': '426px',
+    });
+});
+$(".G").click(function(){
+    $(".letter-highlight").css({
+        'display': 'initial',
+        'margin-left': '465px',
+    });
+});
+$(".H").click(function(){
+    $(".letter-highlight").css({
+        'display': 'initial',
+        'margin-left': '504px',
+    });
+});
+$(".I").click(function(){
+    $(".letter-highlight").css({
+        'display': 'initial',
+        'margin-left': '538px',
+    });
+});
+$(".J").click(function(){
+    $(".letter-highlight").css({
+        'display': 'initial',
+        'margin-left': '565px',
+    });
+});
+$(".K").click(function(){
+    $(".letter-highlight").css({
+        'display': 'initial',
+        'margin-left': '600px',
+    });
+});
+$(".L").click(function(){
+    $(".letter-highlight").css({
+        'display': 'initial',
+        'margin-left': '637px',
+    });
+});
+$(".M").click(function(){
+    $(".letter-highlight").css({
+        'display': 'initial',
+        'margin-left': '678px',
+    });
+});
+$(".N").click(function(){
+    $(".letter-highlight").css({
+        'display': 'initial',
+        'margin-left': '723px',
+    });
+});
+$(".O").click(function(){
+    $(".letter-highlight").css({
+        'display': 'initial',
+        'margin-left': '766px',
+    });
+});
+$(".P").click(function(){
+    $(".letter-highlight").css({
+        'display': 'initial',
+        'margin-left': '804px',
+    });
+});
+$(".Q").click(function(){
+    $(".letter-highlight").css({
+        'display': 'initial',
+        'margin-left': '844px',
+    });
+});
+$(".R").click(function(){
+    $(".letter-highlight").css({
+        'display': 'initial',
+        'margin-left': '883px',
+    });
+});
+$(".S").click(function(){
+    $(".letter-highlight").css({
+        'display': 'initial',
+        'margin-left': '919px',
+    });
+});
+$(".T").click(function(){
+    $(".letter-highlight").css({
+        'display': 'initial',
+        'margin-left': '955px',
+    });
+});
+$(".U").click(function(){
+    $(".letter-highlight").css({
+        'display': 'initial',
+        'margin-left': '992px',
+    });
+});
+$(".V").click(function(){
+    $(".letter-highlight").css({
+        'display': 'initial',
+        'margin-left': '1033px',
+    });
+});
+$(".W").click(function(){
+    $(".letter-highlight").css({
+        'display': 'initial',
+        'margin-left': '1077px',
+    });
+});
+$(".X").click(function(){
+    $(".letter-highlight").css({
+        'display': 'initial',
+        'margin-left': '1120px',
+    });
+});
+$(".Y").click(function(){
+    $(".letter-highlight").css({
+        'display': 'initial',
+        'margin-left': '1160px',
+    });
+});
+$(".Z").click(function(){
+    $(".letter-highlight").css({
+        'display': 'initial',
+        'margin-left': '1195px',
+    });
+});
+
+
+//DROPDOWN
+
+
+
+
+
+
