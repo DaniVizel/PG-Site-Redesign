aff50b2786e5a13326e0c786875320315dcf1819
DaniVizel <daniella.vizelter@student.ocadu.ca>
2021-11-11 03:01:13 -0500

    new version home page

---
+++ script.js
@@ -0,0 +1,9 @@
+// For Bookshelf Menu
+// $(this).children("img:first")
+// https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_collapsible
+
+$(".page-marker").click(function(){
+    $(".nav-bar").toggleClass("nav-bar-open");
+    $(".page-marker").toggleClass("page-marker-open");
+})
+
