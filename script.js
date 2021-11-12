dbfb5435cd77e32dbb8eaea25b27b8cc71a778f6
DaniVizel <daniella.vizelter@student.ocadu.ca>
2021-11-12 04:49:15 -0500

    Made progress on attempt 2

--- script.js
+++ script.js
@@ -3,7 +3,25 @@
 // https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_collapsible
 
 $(".page-marker").click(function(){
-    $(".nav-bar").toggleClass("nav-bar-open");
-    $(".page-marker").toggleClass("page-marker-open");
+    $(".nav-bar").toggleClass("active");
 })
+
+//Bookshelf
+
+$(".letter").click(function(){
+    $(this).toggleClass("active");
+
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+})
+$(".category").click(function(){
+    $(this).toggleClass("active");
+    $(".books-container").css("display", "grid");
+
+})
+
 
